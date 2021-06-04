e5cf8db1c18c6ef5b5a61790d7c6f2784025ef77
Jacob Motes <jmotes2583@gmail.com>
2021-06-03 20:22:46 -0500

    pictures uploaded and server running

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/full-family.jpg', title: 'Family', description: 'The most important thing to me is my family...', likes: 0 },
+    { id: 2, path: 'images/dogs.jpg', title: 'Dogs', description: '...and that includes my two dogs...', likes: 0 },
+    { id: 3, path: 'images/friends.jpg', title: 'Friends', description: '...and friends. Life would not be as good without them.', likes: 0 },
+    { id: 4, path: 'images/time.jpg', title: 'Activity', description: 'I love spending time and doing things with my friends and family.', likes: 0 },
+    { id: 5, path: 'images/travel.jpg', title: 'Travel', description: 'One of my passions is traveling.', likes: 0 },
+    { id: 6, path: 'images/hiking.jpg', title: 'Nature', description: 'I also love nature, hiking, and being outdoors in general.', likes: 0 }
 ];
 
 module.exports = galleryItems;
