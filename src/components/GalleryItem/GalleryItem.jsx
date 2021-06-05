2b5d1fd5d2c23e0c9e5fc40c0b186c5298d977a5
Jacob Motes <jmotes2583@gmail.com>
2021-06-05 16:53:08 -0500

    clicking on picture will now show text

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,15 +3,35 @@ import {useState} from 'react'
 
 function GalleryItem ({picture, getPictures, pictureGallery}) {
     
+    // create a state that can be used to toggle between the image and a description
+    const [ isFlipped, setIsFlipped] = useState(false);
+
+    console.log(isFlipped);
+    
+    // const handleFlip = () => {
+
+    //     if (picture.showingImage == true) {
+    //         return ()
+    //     }
+    // }
     
     
     
     return (
-        <div>
+        <div> 
+            <div className="flex-box-display-child" onClick={ () => setIsFlipped(true)}>
+                {isFlipped ? (
+                <div>
+                    <img src={picture.path} alt="An image of something that is important to me" />
+                </div>
+                ) : (
+                <div>
+                    <p>{picture.description}</p>
+                </div>
+            )}
+            </div>
 
-
-            <div className="flex-box-display-child">
-                <img src={picture.path} alt="An image of something that is important to me" />
+            <div>
                 <button>Like</button>
                 <p>Likes: {picture.likes}</p>
             </div>
